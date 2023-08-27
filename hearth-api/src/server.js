7ccb4b8f3038e08f94c69d7d95bd4ea99bfd001a
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-08-27 12:25:24 +0800

    initial server creation

---
+++ hearth-api/src/server.js
@@ -0,0 +1,19 @@
+import Fastify from 'fastify'
+import UserController from './users_controller.js'
+
+/**
+ * @type {import('fastify').FastifyInstance} Instance of Fastify
+ */
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.register(UserController)
+
+fastify.listen({ port: 8000, host: '0.0.0.0' }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+  })
---
+++ hearth-api/src/users/users_controller.js
@@ -0,0 +1,12 @@
+/**
+ * Encapsulates the routes
+ * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
+ * @param {Object} options plugin options, refer to https://www.fastify.io/docs/latest/Reference/Plugins/#plugin-options
+ */
+export default async function UserController (fastify, options) {
+    fastify.get('/users', async (request, reply) => {
+
+      return { hello: 'world' }
+    })
+  }
+  
