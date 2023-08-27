5b1ea89fdc61862acd9ee06b7d3177b8b6dfad90
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-08-27 17:23:13 +0800

    small changes

--- hearth-api/src/users/users_controller.ts
+++ hearth-api/src/users/users_controller.ts
@@ -1,10 +1,19 @@
+import { FastifyRequest, FastifyReply } from "fastify";
 /**
  * Encapsulates the routes
  * @param {FastifyInstance} fastify  Encapsulated Fastify Instance
  * @param {Object} options plugin options, refer to https://www.fastify.io/docs/latest/Reference/Plugins/#plugin-options
  */
 export default async function UserController(fastify, options) {
-  fastify.get("/users", async (request, reply) => {
-    return { hello: "world" };
-  });
+  fastify.get(
+    "/users",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      reply.code(200).send({ success: true });
+      // but this gives a type error
+      reply.code(200).send("uh-oh");
+      // it even works for wildcards
+      reply.code(404).send({ error: "Not found" });
+      return `logged in!`;
+    }
+  );
 }
