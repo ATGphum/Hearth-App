b3d1eb79854b20249903826660f2ed3a49dc1230
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-08-27 14:46:09 +0800

    added eslint

--- hearth-api/src/server.ts
+++ hearth-api/src/server.ts
@@ -1,19 +1,19 @@
-import Fastify from 'fastify'
-import UserController from './users/users_controller.js'
+import Fastify from "fastify";
+import UserController from "./users/users_controller.js";
 
 /**
  * @type {import('fastify').FastifyInstance} Instance of Fastify
  */
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-fastify.register(UserController)
+fastify.register(UserController);
 
-fastify.listen({ port: 8000, host: '0.0.0.0' }, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    fastify.log.info(`server listening on ${address}`)
-  })
+fastify.listen({ port: 8000, host: "0.0.0.0" }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
