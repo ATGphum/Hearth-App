8c9d020a8e778baaed36e4643ed301131b53fc32
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-08 12:51:45 +0800

    built instagram username enter page

--- hearth-web/src/components/Buttons/FormButton.tsx
+++ hearth-web/src/components/Buttons/FormButton.tsx
@@ -4,14 +4,23 @@ interface Props {
   text: string;
   color?: string;
   backgroundColor?: string;
+  borderColor?: string;
   callback: () => void;
 }
 
-const FormButton = ({ text, color, backgroundColor, callback }: Props) => {
+const FormButton = ({
+  text,
+  color,
+  backgroundColor,
+  borderColor,
+  callback,
+}: Props) => {
   return (
     <Text
       textStyle="action"
       borderRadius="40px"
+      border="1px solid"
+      borderColor={borderColor ?? "neutral.black"}
       padding="0.625rem 2.25rem"
       onClick={callback}
       backgroundColor={backgroundColor ?? "neutral.black"}
--- hearth-web/src/pages/UserCreateForm.tsx
+++ hearth-web/src/pages/UserCreateForm.tsx
@@ -1,87 +1,165 @@
-import { Button, Flex, Input, Text } from "@chakra-ui/react";
-import { Layout } from "../components/Layout";
+import { Flex, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import FormButton from "../components/Buttons/FormButton";
+import { Layout } from "../components/Layout";
 
 function UserCreateForm() {
+  const [page, setPage] = useState(0);
+
   return (
     <Layout>
-      <Flex
-        direction="column"
-        width={"100%"}
-        alignItems={"start"}
-        gridRowGap="1rem"
-        flexGrow={1}
-      >
-        <Text textStyle={"heading.h1"}>Add personal details</Text>
-        <Flex direction={"column"} alignItems="start" width="100%">
-          <Text textStyle="heading.h2">Enter your name</Text>
+      {page === 0 ? (
+        <>
           <Flex
             direction="column"
+            width={"100%"}
             alignItems={"start"}
-            p="1rem"
-            gridRowGap="0.5rem"
-            width="100%"
+            gridRowGap="1rem"
+            flexGrow={1}
           >
-            <Text textStyle="body">First</Text>
-            <Flex
-              borderBottom={"0.5px solid"}
-              borderColor={"accent.navy"}
-              px="0.5rem"
-              width="100%"
-              mb="0.5rem"
-            >
-              <Input variant="unstyled" />
-            </Flex>
-            <Text textStyle="body">Last</Text>
-            <Flex
-              borderBottom={"0.5px solid"}
-              borderColor={"accent.navy"}
-              px="0.5rem"
-              width="100%"
-            >
-              <Input variant="unstyled" />
+            <Text textStyle={"heading.h1"}>Add personal details.</Text>
+            <Flex direction="column" gridRowGap="1rem" px="0.5rem" width="100%">
+              <Flex direction={"column"} alignItems="start" width="100%">
+                <Text textStyle="heading.h2">Enter your name</Text>
+                <Flex
+                  direction="column"
+                  alignItems={"start"}
+                  p="1rem"
+                  gridRowGap="0.5rem"
+                  width="100%"
+                >
+                  <Text textStyle="body">First</Text>
+                  <Flex
+                    borderBottom={"0.5px solid"}
+                    borderColor={"accent.navy"}
+                    px="0.5rem"
+                    width="100%"
+                    mb="0.5rem"
+                  >
+                    <Input variant="unstyled" />
+                  </Flex>
+                  <Text textStyle="body">Last</Text>
+                  <Flex
+                    borderBottom={"0.5px solid"}
+                    borderColor={"accent.navy"}
+                    px="0.5rem"
+                    width="100%"
+                  >
+                    <Input variant="unstyled" />
+                  </Flex>
+                </Flex>
+              </Flex>
+              <Flex direction={"column"} alignItems="start" width="100%">
+                <Text textStyle="heading.h2">Enter your partner's name</Text>
+                <Flex
+                  direction="column"
+                  alignItems={"start"}
+                  p="1rem"
+                  gridRowGap="0.5rem"
+                  width="100%"
+                >
+                  <Text textStyle="body">First</Text>
+                  <Flex
+                    borderBottom={"0.5px solid"}
+                    borderColor={"accent.navy"}
+                    px="0.5rem"
+                    width="100%"
+                    mb="0.5rem"
+                  >
+                    <Input variant="unstyled" />
+                  </Flex>
+                  <Text textStyle="body">Last</Text>
+                  <Flex
+                    borderBottom={"0.5px solid"}
+                    borderColor={"accent.navy"}
+                    px="0.5rem"
+                    width="100%"
+                  >
+                    <Input variant="unstyled" />
+                  </Flex>
+                </Flex>
+              </Flex>
             </Flex>
           </Flex>
-        </Flex>
-        <Flex direction={"column"} alignItems="start" width="100%">
-          <Text textStyle="heading.h2">Enter your name</Text>
+          <Flex
+            width={"100%"}
+            direction={"column"}
+            position={"sticky"}
+            bottom="1rem"
+          >
+            <FormButton
+              text={"Continue"}
+              callback={() => {
+                setPage(1);
+              }}
+            />
+          </Flex>
+        </>
+      ) : page === 1 ? (
+        <>
           <Flex
             direction="column"
+            width={"100%"}
             alignItems={"start"}
-            p="1rem"
-            gridRowGap="0.5rem"
-            width="100%"
+            gridRowGap="1rem"
+            flexGrow={1}
           >
-            <Text textStyle="body">First</Text>
+            <Text textStyle={"heading.h1"}>Let's be social.</Text>
             <Flex
-              borderBottom={"0.5px solid"}
-              borderColor={"accent.navy"}
-              px="0.5rem"
+              direction={"column"}
+              alignItems="start"
               width="100%"
-              mb="0.5rem"
-            >
-              <Input variant="unstyled" />
-            </Flex>
-            <Text textStyle="body">Last</Text>
-            <Flex
-              borderBottom={"0.5px solid"}
-              borderColor={"accent.navy"}
               px="0.5rem"
-              width="100%"
             >
-              <Input variant="unstyled" />
+              <Text textStyle="heading.h2" textAlign={"left"}>
+                We'd love to follow you on Instagram!
+              </Text>
+              <Flex
+                direction="column"
+                alignItems={"start"}
+                p="1rem"
+                gridRowGap="0.5rem"
+                width="100%"
+              >
+                <Text textStyle="body">Your Instagram username</Text>
+                <Flex
+                  borderBottom={"0.5px solid"}
+                  borderColor={"accent.navy"}
+                  px="0.5rem"
+                  width="100%"
+                  mb="0.5rem"
+                >
+                  <Input variant="unstyled" />
+                </Flex>
+              </Flex>
             </Flex>
           </Flex>
-        </Flex>
-      </Flex>
-      <Flex
-        width={"100%"}
-        direction={"column"}
-        position={"sticky"}
-        bottom="1rem"
-      >
-        <FormButton text={"Continue"} />
-      </Flex>
+          <Flex
+            width={"100%"}
+            direction={"column"}
+            position={"sticky"}
+            bottom="1rem"
+            gridRowGap="1rem"
+          >
+            <FormButton
+              text={"Skip this step →"}
+              callback={() => {
+                setPage(2);
+              }}
+              color="neutral.black"
+              backgroundColor="inherit"
+            />
+            <FormButton
+              text={"Continue"}
+              callback={() => {
+                setPage(2);
+              }}
+            />
+          </Flex>
+        </>
+      ) : (
+        <></>
+      )}
     </Layout>
   );
 }
