96973a91c26efd41d50e430c1ebdc07e106eb2e7
Josephine <joworld098@gmail.com>
2023-09-08 22:04:53 +0800

    Adding plugins to make it easier for developing - Used the same plugins from the other project (iykyk)

--- hearth-web/vite.config.ts
+++ hearth-web/vite.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint"; //https://www.npmjs.com/package/vite-plugin-eslint, used this to see the errors and warning in the terminal
+import tsconfigPaths from "vite-tsconfig-paths"; //https://github.com/aleclarson/vite-tsconfig-paths#readme, used to map typescript path -> convenience for development in the frontend
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    {
+      // default settings on build (i.e. fail on error)
+      ...eslint(),
+      apply: "build",
+    },
+    {
+      // do not fail on serve (i.e. local development)
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: "serve",
+      enforce: "post",
+    },
+  ],
   server: {
     watch: {
       usePolling: true,
