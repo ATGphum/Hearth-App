917c478606b71ef38ecdfb7ff8a4370ac0d8921d
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-13 13:19:05 +0800

    Updated library UI

--- hearth-web/src/components/Library.tsx
+++ hearth-web/src/components/Library.tsx
@@ -1,7 +1,12 @@
-import { Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Flex, Image, Text, useDisclosure } from "@chakra-ui/react";
+import MusicDrawer from "./MusicDrawer";
 
 function Library() {
-  const flexString = "1 0 8.5rem";
+  const {
+    isOpen: drawerIsOpen,
+    onOpen: drawerOnOpen,
+    onClose: drawerOnClose,
+  } = useDisclosure();
   return (
     <Flex
       direction="column"
@@ -11,6 +16,7 @@ function Library() {
       pt={0}
       gridRowGap="1rem"
     >
+      <MusicDrawer onClose={drawerOnClose} isOpen={drawerIsOpen} />
       <Flex
         direction="column"
         p="2rem"
@@ -18,101 +24,138 @@ function Library() {
         width="100%"
         borderRadius="2.75rem"
         borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+        onClick={drawerOnOpen}
       >
-        <Text textStyle="heading.h1">Next session</Text>
+        <Image
+          m="1rem"
+          height="6rem"
+          src={
+            "https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png"
+          }
+          objectFit={"contain"}
+        />
+
+        <Text textStyle="heading.h1">Up next</Text>
         <Text textStyle="bodySmall">3-Day Connection Challenge</Text>
         <Text textStyle="heading.h2">Introduction</Text>
         <Text textStyle="body">3 min</Text>
       </Flex>
       <Flex direction="column" textAlign={"left"} gridRowGap="0.75rem">
-        <Text textStyle={"heading.h2"}>Categories</Text>
-        <Text textStyle={"detailText"}>
-          Self-guided options for when you know what kind of topic you want to
-          explore together.
+        <Text textStyle={"heading.h2"}>Connection Journeys</Text>
+        <Text textStyle={"body"}>
+          Directed paths for making conneciton time a habit.
         </Text>
-        <SimpleGrid minChildWidth="8.5rem" spacing="0.75rem">
-          <Flex
-            direction="column"
-            alignItems={"center"}
-            borderRadius="1.5rem"
-            background="linear-gradient(180deg, #BEE6FF 0%, rgba(190, 230, 255, 0.00) 100%)"
-            borderBottom="1px solid rgba(0, 0, 0, 0.40)"
-            p="1rem"
-            flex={flexString}
-          >
-            <Flex py="2rem" />
-            <Text textStyle={"heading.h2"}>Category</Text>
-            <Text textStyle="body">:x: Experiences</Text>
-          </Flex>
-          <Flex
-            direction="column"
-            alignItems={"center"}
-            borderRadius="1.5rem"
-            background="linear-gradient(162deg, #CBE476 12.41%, rgba(203, 228, 118, 0.00) 83.23%)"
-            borderBottom="1px solid rgba(0, 0, 0, 0.40)"
-            p="1rem"
-            flex={flexString}
-          >
-            <Flex py="2rem" />
-            <Text textStyle={"heading.h2"}>Category</Text>
-            <Text textStyle="body">:x: Experiences</Text>
-          </Flex>
-          <Flex
-            direction="column"
-            alignItems={"center"}
-            borderRadius="1.5rem"
-            background="linear-gradient(180deg, #D3F8A7 0%, rgba(211, 248, 167, 0.00) 100%)"
-            borderBottom="1px solid rgba(0, 0, 0, 0.40)"
-            p="1rem"
-            flex={flexString}
-          >
-            <Flex py="2rem" />
-            <Text textStyle={"heading.h2"}>Category</Text>
-            <Text textStyle="body">:x: Experiences</Text>
-          </Flex>
-          <Flex
-            direction="column"
-            alignItems={"center"}
-            borderRadius="1.5rem"
-            background="linear-gradient(174deg, #D1A8FF 4.42%, rgba(209, 167, 255, 0.00) 96.51%)"
-            borderBottom="1px solid rgba(0, 0, 0, 0.40)"
-            p="1rem"
-            flex={flexString}
-          >
-            <Flex py="2rem" />
-            <Text textStyle={"heading.h2"}>Category</Text>
-            <Text textStyle="body">:x: Experiences</Text>
-          </Flex>
-          <Flex
-            direction="column"
-            alignItems={"center"}
-            borderRadius="1.5rem"
-            background="linear-gradient(180deg, #E593DC 0%, rgba(232, 215, 255, 0.00) 100%)"
-            borderBottom="1px solid rgba(0, 0, 0, 0.40)"
-            p="1rem"
-            flex={flexString}
-          >
-            <Flex py="2rem" />
-            <Text textStyle={"heading.h2"}>Category</Text>
-            <Text textStyle="body">:x: Experiences</Text>
-          </Flex>
-          <Flex
-            direction="column"
-            alignItems={"center"}
-            borderRadius="1.5rem"
-            background="linear-gradient(180deg, #51D8AE 0%, rgba(190, 230, 255, 0.00) 100%)"
-            borderBottom="1px solid rgba(0, 0, 0, 0.40)"
-            p="1rem"
-            flex={flexString}
-          >
-            <Flex py="2rem" />
-            <Text textStyle={"heading.h2"}>Category</Text>
-            <Text textStyle="body">:x: Experiences</Text>
-          </Flex>
-        </SimpleGrid>
+        <Flex
+          borderRadius="2.75rem"
+          background="linear-gradient(155deg, rgba(251, 107, 105, 0.50) 15.94%, rgba(251, 107, 105, 0.00) 85.29%)"
+          borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+          p="1rem"
+          alignItems={"center"}
+          gridColumnGap="1rem"
+        >
+          <Image
+            h={"3rem"}
+            src={
+              "https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png"
+            }
+            objectFit={"contain"}
+          />
+          <Text textStyle={"heading.h2"}>Connection Journeys</Text>
+        </Flex>
       </Flex>
     </Flex>
   );
 }
 
 export default Library;
+
+{
+  // const flexString = "1 0 8.5rem";
+  /* <Flex direction="column" textAlign={"left"} gridRowGap="0.75rem">
+<Text textStyle={"heading.h2"}>Categories</Text>
+<Text textStyle={"body"}>
+  Self-guided options for when you know what kind of topic you want to
+  explore together.
+</Text>
+<SimpleGrid minChildWidth="8.5rem" spacing="0.75rem">
+  <Flex
+    direction="column"
+    alignItems={"center"}
+    borderRadius="1.5rem"
+    background="linear-gradient(180deg, #BEE6FF 0%, rgba(190, 230, 255, 0.00) 100%)"
+    borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+    p="1rem"
+    flex={flexString}
+  >
+    <Flex py="2rem" />
+    <Text textStyle={"heading.h2"}>Category</Text>
+    <Text textStyle="body">:x: Experiences</Text>
+  </Flex>
+  <Flex
+    direction="column"
+    alignItems={"center"}
+    borderRadius="1.5rem"
+    background="linear-gradient(162deg, #CBE476 12.41%, rgba(203, 228, 118, 0.00) 83.23%)"
+    borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+    p="1rem"
+    flex={flexString}
+  >
+    <Flex py="2rem" />
+    <Text textStyle={"heading.h2"}>Category</Text>
+    <Text textStyle="body">:x: Experiences</Text>
+  </Flex>
+  <Flex
+    direction="column"
+    alignItems={"center"}
+    borderRadius="1.5rem"
+    background="linear-gradient(180deg, #D3F8A7 0%, rgba(211, 248, 167, 0.00) 100%)"
+    borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+    p="1rem"
+    flex={flexString}
+  >
+    <Flex py="2rem" />
+    <Text textStyle={"heading.h2"}>Category</Text>
+    <Text textStyle="body">:x: Experiences</Text>
+  </Flex>
+  <Flex
+    direction="column"
+    alignItems={"center"}
+    borderRadius="1.5rem"
+    background="linear-gradient(174deg, #D1A8FF 4.42%, rgba(209, 167, 255, 0.00) 96.51%)"
+    borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+    p="1rem"
+    flex={flexString}
+  >
+    <Flex py="2rem" />
+    <Text textStyle={"heading.h2"}>Category</Text>
+    <Text textStyle="body">:x: Experiences</Text>
+  </Flex>
+  <Flex
+    direction="column"
+    alignItems={"center"}
+    borderRadius="1.5rem"
+    background="linear-gradient(180deg, #E593DC 0%, rgba(232, 215, 255, 0.00) 100%)"
+    borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+    p="1rem"
+    flex={flexString}
+  >
+    <Flex py="2rem" />
+    <Text textStyle={"heading.h2"}>Category</Text>
+    <Text textStyle="body">:x: Experiences</Text>
+  </Flex>
+  <Flex
+    direction="column"
+    alignItems={"center"}
+    borderRadius="1.5rem"
+    background="linear-gradient(180deg, #51D8AE 0%, rgba(190, 230, 255, 0.00) 100%)"
+    borderBottom="1px solid rgba(0, 0, 0, 0.40)"
+    p="1rem"
+    flex={flexString}
+  >
+    <Flex py="2rem" />
+    <Text textStyle={"heading.h2"}>Category</Text>
+    <Text textStyle="body">:x: Experiences</Text>
+  </Flex>
+</SimpleGrid>
+</Flex> */
+}
