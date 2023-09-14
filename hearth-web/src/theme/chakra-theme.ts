b1e4550bb2098b01c4f0b9e217dac5d0152ee363
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-14 16:05:49 +0800

    re-added slider import from chakra

--- hearth-web/src/theme/chakra-theme.ts
+++ hearth-web/src/theme/chakra-theme.ts
@@ -2,23 +2,23 @@
 
 import { extendBaseTheme } from "@chakra-ui/react";
 import "@fontsource/inter";
-import "@fontsource/inter/300.css";
 import "@fontsource/inter/500.css";
-import "@fontsource/jost";
+import "@fontsource/inter/300.css";
 import "@fontsource/lato/700.css";
+import "@fontsource/jost";
 import colors from "./colors";
 import textStyles from "./text-styles";
 
-/**
- * This theme config allows you to add variables for use in Chakra components
- * To begin with I've added the themed color values Joe has setup in figma
- */
+import chakraTheme from "@chakra-ui/theme";
+const { Slider } = chakraTheme.components;
 
 const theme = {
   fonts: { heading: `Jost, "Inter"`, body: `Jost, "Inter"` },
   textStyles: textStyles,
   colors: colors,
-
+  components: {
+    Slider,
+  },
   config: {
     useSystemColorMode: false,
   },
