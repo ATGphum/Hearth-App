b64e7900ae8e8ae454042580c31994518742dec7
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-15 01:40:08 +0800

    re-introduced loading page

--- hearth-web/src/components/Layout.tsx
+++ hearth-web/src/components/Layout.tsx
@@ -1,9 +1,9 @@
-// import { useContext, useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { UserContext } from "../context/UserContext";
-// import LoadingPage from "../pages/LoadingPage";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import LoadingPage from "../pages/LoadingPage";
 import { LayoutNoRedirect } from "./LayoutNoRedirect";
-// import { useJourneys } from "../core/apiHooks";
+import { useJourneys } from "../core/apiHooks";
 
 interface Props {
   children: React.ReactNode;
@@ -11,32 +11,32 @@ interface Props {
 }
 
 export const Layout = ({ children, hidePadding }: Props) => {
-  // const navigate = useNavigate();
-  // const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext);
 
-  // const { data: journeys } = useJourneys();
+  const { data: journeys } = useJourneys();
 
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 2000); // 2000ms = 2 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2000ms = 2 seconds
 
-  //   return () => clearTimeout(timer);
-  // }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-  // // show loading page while user data is retrieving or 2 seconds is up
-  // // if (!user || !journeys || isLoading) return <LoadingPage />;
+  // show loading page while user data is retrieving or 2 seconds is up
+  if (!user || !journeys || isLoading) return <LoadingPage />;
 
-  // // show user form if essential fields are not present
-  // if (
-  //   !user.first_name ||
-  //   !user.last_name ||
-  //   !user.partner_first_name ||
-  //   !user.partner_last_name
-  // )
-  //   navigate("/create-user");
+  // show user form if essential fields are not present
+  if (
+    !user.first_name ||
+    !user.last_name ||
+    !user.partner_first_name ||
+    !user.partner_last_name
+  )
+    navigate("/create-user");
 
   return (
     <LayoutNoRedirect hidePadding={hidePadding}>{children}</LayoutNoRedirect>
