047a58c4a6619115d7ca950f1a31ada84e1610b0
Josephine <joworld098@gmail.com>
2023-09-16 10:04:03 +0800

    Remove option

--- hearth-api/src/config/fastify-cors.ts
+++ hearth-api/src/config/fastify-cors.ts
@@ -2,7 +2,7 @@ import fastifyEnv from "./fastify-env.js";
 
 const corsObj = {
   origin: [fastifyEnv.frontendHost], // Adjust to your React app's URL
-  methods: ["GET", "POST", "DELETE", "PATCH", "OPTION"], // Allow specific HTTP methods
+  methods: ["GET", "POST", "DELETE", "PATCH"], // Allow specific HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   credentials: true, // Enable sending cookies and other credentials
 };
