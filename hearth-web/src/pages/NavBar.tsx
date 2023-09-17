5cb2fe44d4ce99b6a3bfc33fc6f73b8e55e90bbd
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-17 15:45:28 +0800

    added temporary icons to navbar

--- hearth-web/src/pages/NavBar.tsx
+++ hearth-web/src/pages/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 
 interface navProps {
   tabChange: (pageNumber: number) => void;
@@ -7,20 +7,50 @@ interface navProps {
 const NavBar = ({ tabChange }: navProps) => {
   return (
     <Flex
-      justifyContent={"space-around"}
       width="100%"
       position="sticky"
       bottom={0}
       bg="linear-gradient(148deg, rgba(250, 151, 4, 0.25) 19.23%, rgba(250, 151, 4, 0.00) 81.73%)"
+      maxW="40rem"
+      alignSelf={"flex-start"}
+      alignItems={"center"}
+      pb="2rem"
     >
-      <Flex p="1rem" onClick={() => tabChange(0)}>
-        1
+      <Flex
+        p="1rem"
+        onClick={() => tabChange(0)}
+        flex={1}
+        justifyContent={"center"}
+      >
+        <Image
+          src={
+            "https://res.cloudinary.com/ddh1fblle/image/upload/v1694936418/Property_1_tape_tuo8yr.png"
+          }
+        />
       </Flex>
-      <Flex p="1rem" onClick={() => tabChange(1)}>
-        2
+      <Flex
+        p="1rem"
+        onClick={() => tabChange(1)}
+        flex={1}
+        justifyContent={"center"}
+      >
+        <Image
+          src={
+            "https://res.cloudinary.com/ddh1fblle/image/upload/v1694936395/Untitled_Artwork_43_2_xcsubw.png"
+          }
+        />
       </Flex>
-      <Flex p="1rem" onClick={() => tabChange(2)}>
-        3
+      <Flex
+        p="1rem"
+        onClick={() => tabChange(2)}
+        flex={1}
+        justifyContent={"center"}
+      >
+        <Image
+          src={
+            "https://res.cloudinary.com/ddh1fblle/image/upload/v1694936419/Property_1_Variant16_hnecni.png"
+          }
+        />
       </Flex>
     </Flex>
   );
