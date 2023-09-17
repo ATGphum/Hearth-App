e972a35cd4c3e46dce1c39108c7ecc29993cc7d3
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-17 13:10:45 +0800

    reverted auth logic

--- hearth-web/src/App.tsx
+++ hearth-web/src/App.tsx
@@ -5,7 +5,6 @@ import RenderRoutes from "./Routes";
 import { UserProvider } from "./context/UserContext";
 import { request } from "./core/api";
 import theme from "./theme/chakra-theme";
-import AccessTokenProvider from "./context/AccessTokenProvider";
 
 function App() {
   return (
@@ -24,29 +23,27 @@ interface ProviderProps {
 const AppContextProviders = ({ children }: ProviderProps) => {
   return (
     <ChakraBaseProvider theme={theme}>
-      <AccessTokenProvider>
-        <SWRConfig
-          value={{
-            fetcher: async (url: string) => {
-              return request<any>(url, "GET").then((res) => {
-                if (res.status >= 300) {
-                  const error = new Error(
-                    "An error occurred while fetching the data."
-                  );
-                  // Attach extra info to the error object.
-                  // eslint-disable-next-line
-                  // @ts-ignore
-                  error.status = res.status;
-                  throw error;
-                }
-                return res.data;
-              });
-            },
-          }}
-        >
-          <UserProvider>{children}</UserProvider>
-        </SWRConfig>
-      </AccessTokenProvider>
+      <SWRConfig
+        value={{
+          fetcher: async (url: string) => {
+            return request<any>(url, "GET").then((res) => {
+              if (res.status >= 300) {
+                const error = new Error(
+                  "An error occurred while fetching the data."
+                );
+                // Attach extra info to the error object.
+                // eslint-disable-next-line
+                // @ts-ignore
+                error.status = res.status;
+                throw error;
+              }
+              return res.data;
+            });
+          },
+        }}
+      >
+        <UserProvider>{children}</UserProvider>
+      </SWRConfig>
     </ChakraBaseProvider>
   );
 };
