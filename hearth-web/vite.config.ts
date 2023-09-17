47cdbeb189bb22382c3eb87044776b1f00aed99b
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-17 17:29:20 +0800

    added workbox caching rules

--- hearth-web/vite.config.ts
+++ hearth-web/vite.config.ts
@@ -4,7 +4,15 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,mp3}"],
+      },
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
