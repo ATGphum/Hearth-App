af07674ec9386a5f8f2d153516eddaaf7d5196a3
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-17 13:56:49 +0800

    import only relevant fonts

--- hearth-web/src/theme/chakra-theme.ts
+++ hearth-web/src/theme/chakra-theme.ts
@@ -1,7 +1,7 @@
 // https://chakra-ui.com/docs/features/color-mode#updating-the-theme-configimport
 
 import { extendBaseTheme } from "@chakra-ui/react";
-import "@fontsource/inter";
+import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/300.css";
 import "@fontsource/lato/700.css";
