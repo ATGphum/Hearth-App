a56f8f07020a2fe90392b85ee2ce2a41fa0fded3
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-18 18:48:02 +0800

    reverted token storage to memory

--- hearth-web/src/main.tsx
+++ hearth-web/src/main.tsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           scope: viteEnv.auth0.scope,
         }}
         useRefreshTokens={true}
-        cacheLocation="localstorage"
+        cacheLocation="memory"
       >
         <App />
       </Auth0Provider>
