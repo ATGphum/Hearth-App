cd398e05f0af7a6c2a6e61a256b7e28c77ac6f7e
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-18 19:13:41 +0800

    updated loading page

--- hearth-web/src/pages/LoadingPage.tsx
+++ hearth-web/src/pages/LoadingPage.tsx
@@ -1,36 +1,23 @@
-import { Flex, keyframes } from "@chakra-ui/react";
-import { m } from "framer-motion";
-import { LayoutNoRedirect } from "../components/LayoutNoRedirect";
+import { Flex, Image } from "@chakra-ui/react";
 
 function LoadingPage() {
-  const animationKeyframes = keyframes`
-  0% { transform: scale(1) rotate(0); border-radius: 20%; }
-  25% { transform: scale(3) rotate(0); border-radius: 20%; }
-  50% { transform: scale(3) rotate(270deg); border-radius: 50%; }
-  75% { transform: scale(1) rotate(270deg); border-radius: 50%; }
-  100% { transform: scale(1) rotate(0); border-radius: 20%; }
-`;
-
-  const animation = `${animationKeyframes} 2s ease-in-out infinite`;
-
   return (
-    <LayoutNoRedirect>
-      <Flex flex="1" alignItems="center" justifyContent="center">
-        <Flex pb="2rem">
-          <Flex
-            as={m.div}
-            animation={animation}
-            padding="2"
-            bgGradient="linear(to-l, hsla(296, 94%, 67%, 1), hsla(242, 76%, 56%, 1))"
-            width="4rem"
-            height="4rem"
-            display="flex"
-            drag={true}
-            borderRadius={"20%"}
-          />
-        </Flex>
-      </Flex>
-    </LayoutNoRedirect>
+    <Flex
+      direction="column"
+      alignItems="center"
+      background="linear-gradient(180deg, #FFBB79 2.78%, #FFDEC0 31.35%, #FFDEC0 98.99%, #FFDEC0 98.99%)"
+      height="100vh"
+      position="relative"
+      overflow={"hidden"}
+      justifyContent={"center"}
+    >
+      <Image
+        src={
+          "https://res.cloudinary.com/ddh1fblle/image/upload/v1694933705/Web_ilczox.svg"
+        }
+        alt="Hearth"
+      />
+    </Flex>
   );
 }
 
