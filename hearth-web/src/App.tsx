77f20144a873603727a90b906c8970e109b02da7
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-19 15:02:34 +0800

    updated app structure

--- hearth-web/src/App.tsx
+++ hearth-web/src/App.tsx
@@ -5,6 +5,8 @@ import RenderRoutes from "./Routes";
 import { UserProvider } from "./context/UserContext";
 import { request } from "./core/api";
 import theme from "./theme/chakra-theme";
+import { Auth0Provider } from "@auth0/auth0-react";
+import viteEnv from "./config/vite-env";
 
 function App() {
   return (
@@ -22,28 +24,44 @@ interface ProviderProps {
 
 const AppContextProviders = ({ children }: ProviderProps) => {
   return (
-    <ChakraBaseProvider theme={theme}>
-      <SWRConfig
-        value={{
-          fetcher: async (url: string) => {
-            return request<any>(url, "GET").then((res) => {
-              if (res.status >= 300) {
-                const error = new Error(
-                  "An error occurred while fetching the data."
-                );
-                // Attach extra info to the error object.
-                // eslint-disable-next-line
-                // @ts-ignore
-                error.status = res.status;
-                throw error;
-              }
-              return res.data;
-            });
-          },
+    <div className="scrollable-content">
+      <Auth0Provider
+        domain={viteEnv.auth0.domain}
+        clientId={viteEnv.auth0.hearthWeb.id}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: viteEnv.auth0.api.audience,
+          scope: viteEnv.auth0.scope,
         }}
+        useRefreshTokens={true}
+        cacheLocation="localstorage"
       >
-        <UserProvider>{children}</UserProvider>
-      </SWRConfig>
-    </ChakraBaseProvider>
+        <div className="scrollable-content">
+          <ChakraBaseProvider theme={theme}>
+            <SWRConfig
+              value={{
+                fetcher: async (url: string) => {
+                  return request<any>(url, "GET").then((res) => {
+                    if (res.status >= 300) {
+                      const error = new Error(
+                        "An error occurred while fetching the data."
+                      );
+                      // Attach extra info to the error object.
+                      // eslint-disable-next-line
+                      // @ts-ignore
+                      error.status = res.status;
+                      throw error;
+                    }
+                    return res.data;
+                  });
+                },
+              }}
+            >
+              <UserProvider>{children}</UserProvider>
+            </SWRConfig>
+          </ChakraBaseProvider>
+        </div>
+      </Auth0Provider>
+    </div>
   );
 };
--- hearth-web/src/main.tsx
+++ hearth-web/src/main.tsx
@@ -7,22 +7,6 @@ import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="scrollable-content">
-      <Auth0Provider
-        domain={viteEnv.auth0.domain}
-        clientId={viteEnv.auth0.hearthWeb.id}
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-          audience: viteEnv.auth0.api.audience,
-          scope: viteEnv.auth0.scope,
-        }}
-        useRefreshTokens={true}
-        cacheLocation="localstorage"
-      >
-        <div className="scrollable-content">
-          <App />
-        </div>
-      </Auth0Provider>
-    </div>
+    <App />
   </StrictMode>
 );
