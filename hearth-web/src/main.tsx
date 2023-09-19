d8a0754c0cdb3b113a0e022a05fd9f6a7a026ea7
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-19 15:03:50 +0800

    removed unused imports

--- hearth-web/src/main.tsx
+++ hearth-web/src/main.tsx
@@ -1,9 +1,7 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Auth0Provider } from "@auth0/auth0-react";
-import viteEnv from "./config/vite-env.ts";
-import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
