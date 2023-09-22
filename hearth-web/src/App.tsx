53338f0c0c78f3af6e8b3da3dc6d034182d756e5
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-09-22 14:00:54 +0800

    reconfigured files to decrease loading time

--- hearth-web/src/App.tsx
+++ hearth-web/src/App.tsx
@@ -1,15 +1,13 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-import { ChakraBaseProvider } from "@chakra-ui/react";
-import { SWRConfig } from "swr";
+import { Suspense, lazy } from "react";
 import "./App.css";
-import { Tree } from "./components/Tree";
+import Tree from "./components/Tree";
 import viteEnv from "./config/vite-env";
-import { UserProvider } from "./context/UserContext";
-import { request } from "./core/api";
 import { IsStandalone, getInstallableStatus } from "./core/helpers";
-import DesktopPage from "./pages/DesktopPage";
-import theme from "./theme/chakra-theme";
-import WrongBrowserPage from "./pages/WrongBrowserPage";
+import LoadingPage from "./pages/LoadingPage";
+
+const DesktopPage = lazy(() => import("./pages/DesktopPage"));
+const WrongBrowserPage = lazy(() => import("./pages/WrongBrowserPage"));
+const AppContextProviders = lazy(() => import("./context/AppContextProviders"));
 
 function App() {
   const installable = getInstallableStatus();
@@ -17,71 +15,26 @@ function App() {
   if (installable === "installable") {
     if (isStandalone || viteEnv.environment === "development") {
       return (
-        <AppContextProviders>
-          <Tree />
-        </AppContextProviders>
+        <Suspense fallback={<LoadingPage />}>
+          <AppContextProviders>
+            <Tree />
+          </AppContextProviders>
+        </Suspense>
       );
     } else {
       return (
-        <ChakraBaseProvider theme={theme}>
+        <Suspense fallback={<LoadingPage />}>
           <WrongBrowserPage />
-        </ChakraBaseProvider>
+        </Suspense>
       );
     }
   }
 
   return (
-    <ChakraBaseProvider theme={theme}>
+    <Suspense fallback={<LoadingPage />}>
       <DesktopPage />
-    </ChakraBaseProvider>
+    </Suspense>
   );
 }
 
 export default App;
-
-interface ProviderProps {
-  children: React.ReactNode;
-}
-
-const AppContextProviders = ({ children }: ProviderProps) => {
-  return (
-    <Auth0Provider
-      domain={viteEnv.auth0.domain}
-      clientId={viteEnv.auth0.hearthWeb.id}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: viteEnv.auth0.api.audience,
-        scope: viteEnv.auth0.scope,
-      }}
-      cacheLocation="localstorage"
-    >
-      <div className="unscrollable-content">
-        <div className="scrollable-content">
-          <ChakraBaseProvider theme={theme}>
-            <SWRConfig
-              value={{
-                fetcher: async (url: string) => {
-                  return request<any>(url, "GET").then((res) => {
-                    if (res.status >= 300) {
-                      const error = new Error(
-                        "An error occurred while fetching the data."
-                      );
-                      // Attach extra info to the error object.
-                      // eslint-disable-next-line
-                      // @ts-ignore
-                      error.status = res.status;
-                      throw error;
-                    }
-                    return res.data;
-                  });
-                },
-              }}
-            >
-              <UserProvider>{children}</UserProvider>
-            </SWRConfig>
-          </ChakraBaseProvider>
-        </div>
-      </div>
-    </Auth0Provider>
-  );
-};
