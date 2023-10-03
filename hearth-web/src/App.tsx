c8ba73605c521a195be6e8b7068d7fe71f1d0f07
Josephine <joworld098@gmail.com>
2023-10-03 21:05:51 +0800

    Fix bug logic - use environment development - Rather than prod

--- hearth-web/src/App.tsx
+++ hearth-web/src/App.tsx
@@ -19,8 +19,8 @@ function App() {
     initialiseAnalytics();
   }, []);
 
-  if (installable === "installable" || viteEnv.environment === "prod") {
-    if (isStandalone || viteEnv.environment === "prod") {
+  if (installable === "installable" || viteEnv.environment === "development") {
+    if (isStandalone || viteEnv.environment === "development") {
       return (
         <Suspense fallback={<LoadingPage />}>
           <AppContextProviders>
