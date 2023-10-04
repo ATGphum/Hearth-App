d3cf8cca9f8c56cba07fdd050b04ee93cbfbbc8f
Josephine <joworld098@gmail.com>
2023-10-04 19:01:09 +0800

    Added section 5

--- hearth-web/src/pages/TermsAndConditionsPage.tsx
+++ hearth-web/src/pages/TermsAndConditionsPage.tsx
@@ -1,4 +1,4 @@
-import { Flex, ListItem, OrderedList, Text } from "@chakra-ui/react";
+import { Flex, List, ListItem, OrderedList, Text } from "@chakra-ui/react";
 import { LazyMotion, domMax, m } from "framer-motion";
 import ReactDOM from "react-dom";
 import { trackEvent } from "../core/analytics";
@@ -253,7 +253,7 @@ const TermsAndConditionsPage = ({ isOpen, onClose }: Props) => {
               >
                 <ListItem mt="1rem">
                   <Text textStyle="body">
-                    Except with the written consent of Health , you may only use
+                    Except with the written consent of Health, you may only use
                     the Services (including the Content) for personal
                     non-commercial purposes.{" "}
                   </Text>
@@ -292,6 +292,100 @@ const TermsAndConditionsPage = ({ isOpen, onClose }: Props) => {
             </ListItem>
             <ListItem>
               <Text textStyle="heading.h2">User Account</Text>
+            </ListItem>
+            <ListItem
+              as="div"
+              textStyle="heading.h3"
+              position="relative"
+              _before={{
+                content: "'5.1'",
+                position: "absolute",
+                left: "-1.5rem",
+              }}
+            >
+              <Text textStyle="heading.h3" ml="0.25rem">
+                Use of User Account
+              </Text>
+              <OrderedList
+                listStyleType="lower-alpha"
+                textStyle="body"
+                spacing="1rem"
+              >
+                <ListItem mt="1rem">
+                  <Text textStyle="body">
+                    To be eligible for a User Account you must be an individual
+                    over the age of 18 years and capable of forming a binding
+                    contract (
+                    <Text as="span" fontWeight={500}>
+                      Eligibility Criteria
+                    </Text>
+                    ). You may use the Services only if you are 13 years or
+                    older and are not barred from using the Services under
+                    applicable law.{" "}
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    Subject to satisfying the Eligibility Criteria, you may
+                    apply for a “user account” to access the Services (
+                    <Text as="span" fontWeight={500}>
+                      User Account
+                    </Text>
+                    ). By applying for a User Account you warrant and represent
+                    that you satisfy the Eligibility Criteria. If at any time
+                    you cease to satisfy the Eligibility Criteria you must cease
+                    accessing and using the Services and Hearth may terminate
+                    your User Account.
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    If Hearth, at its absolute discretion, grants you a User
+                    Account, your use of that account is subject to and
+                    conditional on your compliance with these Terms.
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    If you choose, or you are provided with, a username, user
+                    identification code, password or any other piece of
+                    information as part of our security procedures (
+                    <Text as="span" fontWeight={500}>
+                      Security Information
+                    </Text>
+                    ), you must treat such information as confidential. You must
+                    not disclose it to any third party. You are responsible for
+                    the safekeeping of the Security Information, and liable if
+                    your account is misused by an unauthorised person.
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    We have the right to disable any of the Security
+                    Information, whether chosen by you or allocate by us, at any
+                    time, if in our reasonable opinion you have failed to comply
+                    with any of the provisions of these Terms.{" "}
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    You are responsible for ensuring that all persons who access
+                    the Services through your internet connection are aware of
+                    the Terms and other applicable terms and conditions, and
+                    that they comply with them. If you know or suspect that
+                    anyone other than you know any of your Security Information,
+                    you must promptly notify us by emailing
+                    hello@hearthtogether.com.
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    Notwithstanding any clause to the contrary, we reserve the
+                    right to refuse to set up a User Account or provide a
+                    Service to anyone for any reason at any time.{" "}
+                  </Text>
+                </ListItem>
+              </OrderedList>
             </ListItem>
             <ListItem>
               <Text textStyle="heading.h2">Purchasing Services</Text>
