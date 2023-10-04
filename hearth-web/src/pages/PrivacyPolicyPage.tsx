35f9f8bff9b81a5ef080300a0f0579f50f011ae8
Josephine <joworld098@gmail.com>
2023-10-04 21:23:27 +0800

    Added contents up to section 8

--- hearth-web/src/pages/PrivacyPolicyPage.tsx
+++ hearth-web/src/pages/PrivacyPolicyPage.tsx
@@ -372,117 +372,282 @@ const PrivacyPolicyPage = ({ isOpen, onClose }: Props) => {
               <Text textStyle="heading.h2">
                 Why do we collect personal information?{" "}
               </Text>
-              <OrderedList
-                listStyleType="lower-alpha"
-                textStyle="body"
-                spacing="1rem"
-              >
+            </ListItem>
+            <ListItem
+              as="div"
+              textStyle="heading.h3"
+              position="relative"
+              _before={{
+                content: "'5.1'",
+                position: "absolute",
+                left: "-1.5rem",
+              }}
+            >
+              <Text textStyle="heading.h3" ml="0.25rem">
+                Directly from you
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We collect personal information directly from you when you
+                register for a User Account, to access our Services or otherwise
+                engage with us.{" "}
+              </Text>
+            </ListItem>
+            <ListItem
+              as="div"
+              textStyle="heading.h3"
+              position="relative"
+              _before={{
+                content: "'5.2'",
+                position: "absolute",
+                left: "-1.5rem",
+              }}
+            >
+              <Text textStyle="heading.h3" ml="0.25rem">
+                Information from third parties
+              </Text>
+              <br />
+              <Text textStyle="body">
+                When a Service is purchased as a gift, we will collect personal
+                information from a third party, this is usually your friends and
+                relatives.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We also obtain information about you from other sources, such as
+                third-party app stores/providers or social media service
+                accounts where you have elected to link or use with our
+                Services.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We may also obtain information about you from publicly available
+                sources.
+              </Text>
+            </ListItem>
+            <ListItem
+              as="div"
+              textStyle="heading.h3"
+              position="relative"
+              _before={{
+                content: "'5.3'",
+                position: "absolute",
+                left: "-1.5rem",
+              }}
+            >
+              <Text textStyle="heading.h3" ml="0.25rem">
+                Unsolicited information
+              </Text>
+              <br />
+              <Text textStyle="body">
+                If we receive your personal information from you or third
+                parties which we did not request, we will determine whether we
+                are permitted to collect the information in accordance with the
+                Privacy Act.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                If we determine that we are not permitted to collect that
+                personal information, we will, as soon as practicable, lawfully
+                destroy the information or ensure that it is de-identified. If
+                we determine that we are permitted to collect the personal
+                information, we will ensure that the information is handled with
+                in a manner that complies with our obligations under Australian
+                Privacy Law.
+              </Text>
+            </ListItem>
+            <ListItem
+              as="div"
+              textStyle="heading.h3"
+              position="relative"
+              _before={{
+                content: "'5.4'",
+                position: "absolute",
+                left: "-1.5rem",
+              }}
+            >
+              <Text textStyle="heading.h3" ml="0.25rem">
+                Anonymity and pseudonymity
+              </Text>
+              <br />
+              <Text textStyle="body">
+                When interacting with us, you may choose to remain anonymous or
+                to use a pseudonym. However, in some circumstances, it may not
+                be possible for us to properly provide a service without the
+                knowledge of your identity.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We may elect not to deal with you anonymously or pseudonymously
+                if:
+              </Text>
+              <UnorderedList textStyle="body" spacing="1rem" ml="2rem">
                 <ListItem mt="1rem">
                   <Text textStyle="body">
-                    BetterLabs Pty Ltd (ACN 620 505 615) trading as Health (
-                    <Text as="span" fontWeight={500}>
-                      Hearth
-                    </Text>
-                    ,{" "}
-                    <Text as="span" fontWeight={500}>
-                      we
-                    </Text>
-                    ,{" "}
-                    <Text as="span" fontWeight={500}>
-                      us
-                    </Text>{" "}
-                    and{" "}
-                    <Text as="span" fontWeight={500}>
-                      our
-                    </Text>
-                    ) is the owner or licensee of:{" "}
+                    we are required or authorised by or under Law, or a
+                    court/tribunal order, to deal with you in accordance with
+                    your identity; or
                   </Text>
-                  <OrderedList
-                    listStyleType="lower-roman"
-                    textStyle="body"
-                    spacing="0.5rem"
-                  >
-                    <ListItem mt="0.5rem">
-                      <Text textStyle="body">the Services; and</Text>
-                    </ListItem>
-                    <ListItem>
-                      <Text textStyle="body">
-                        all intellectual property rights in the Services, and in
-                        the material published on it (including but not limited
-                        to all data, text, software, images, graphics,
-                        trademarks, logos, interfaces, photographs) (
-                        <Text as="span" fontWeight={500}>
-                          Content
-                        </Text>
-                        ).
-                      </Text>
-                    </ListItem>
-                  </OrderedList>
                 </ListItem>
                 <ListItem>
                   <Text textStyle="body">
-                    Hearth reserves all rights in and to the Services, not
-                    expressly granted to you under these Terms.
+                    it is impracticable for us to deal with you in this way.
                   </Text>
                 </ListItem>
-              </OrderedList>
+              </UnorderedList>
             </ListItem>
             <ListItem>
               <Text textStyle="heading.h2">
                 How do we collect this information?{" "}
               </Text>
-              <OrderedList
-                listStyleType="lower-alpha"
-                textStyle="body"
-                spacing="1rem"
-              >
+              <br />
+              <Text textStyle="body">
+                Subject to this Policy and the Law, we will use or disclose your
+                personal information for the purpose it was collected.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We will only use or disclose your personal information for
+                another purpose in the following circumstances:{" "}
+              </Text>
+              <UnorderedList textStyle="body" spacing="1rem" ml="2rem">
+                <ListItem mt="1rem">
+                  <Text textStyle="body">where you have consented; </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    where the use or disclosure is directly related to the
+                    Services or reason you engaged with us and you would
+                    reasonably expect us to use or disclose your personal
+                    information in this way (e.g. service management,
+                    administration, billing, audit and quality assurance,
+                    accreditation, incident monitoring); or{" "}
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text textStyle="body">
+                    as required or authorised by law or court/tribunal order.
+                  </Text>
+                </ListItem>
+              </UnorderedList>
+              <br />
+              <Text textStyle="body">
+                We may share information with our related and subsidiary
+                companies to help us provide you with more efficient service and
+                to keep you informed about our products and services.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We may also share your information with unrelated third party
+                companies where those companies provide services to us that
+                enable us to undertake our functions (such as those that assist
+                us with advertising measurement, communications and surveys,
+                data analytics, fraud prevention, cloud storage, bug fix
+                management and payment processing.) Please note, we will only
+                disclose your personal information to third party companies with
+                the condition that they will comply with Australian Privacy Law.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We will never commercialise or sell your personal information.
+                However, if ownership or control of all or part of our business
+                changes, we may transfer your personal information to a
+                potential or new owner.{" "}
+              </Text>
+              <br />
+              <Text textStyle="body">
+                Occasionally, we may disclose your personal details to market
+                research companies engaged by us to gather independent feedback
+                from you about the performance of our Services.
+              </Text>
+              <br />
+              <Text textStyle="body">Please contact us, if at any time:</Text>
+              <UnorderedList textStyle="body" spacing="1rem" ml="2rem">
                 <ListItem mt="1rem">
                   <Text textStyle="body">
-                    Except with the written consent of Health, you may only use
-                    the Services (including the Content) for personal
-                    non-commercial purposes.{" "}
+                    you would like to know more information about which
+                    companies we regularly deal with and what personal
+                    information they receive from us; or
                   </Text>
                 </ListItem>
                 <ListItem>
                   <Text textStyle="body">
-                    The Services and the Content are directed to users who are
-                    at least 18 years old, residing within Australia. We do not
-                    represent that the Content is appropriate for use or
-                    available in other locations. If you access the Services
-                    outside Australia, you do so at your own risk and you are
-                    responsible for compliance with laws applicable to your
-                    accessing the Services from your location.{" "}
+                    you wish to stop your personal information from being used
+                    or disclosed for the purposes set out in this Policy.
                   </Text>
                 </ListItem>
-                <ListItem>
-                  <Text textStyle="body">
-                    You may only use the mobile app feature of the Services
-                    subject to these Terms. Hearth grants you a limited,
-                    non-exclusive, non-transferable, non-sublicensable license
-                    to download and install a copy of the mobile app on a mobile
-                    device or computer that you own or control and to run such
-                    copy of the mobile app solely for your own personal
-                    non-commercial purposes.{" "}
-                  </Text>
-                </ListItem>
-                <ListItem>
-                  <Text textStyle="body">
-                    Additional terms and conditions may also apply to specific
-                    parts or features of the Services. All such additional terms
-                    and conditions are incorporated by this reference into these
-                    Terms.
-                  </Text>
-                </ListItem>
-              </OrderedList>
+              </UnorderedList>
             </ListItem>
             <ListItem>
               <Text textStyle="heading.h2">
                 How do we disclose your personal information?
               </Text>
+              <br />
+              <Text textStyle="body">
+                Sometimes our suppliers, contractors or agents are based
+                overseas, or otherwise have data storage facilities overseas
+                where your personal information will be stored. We may transfer
+                information about you between countries if required for a
+                relevant purpose described above. However, in all cases, we will
+                take reasonable steps to ensure all entities to whom we transfer
+                your personal information comply with the Australian Privacy
+                Law, including ensuring appropriate security measures are taken
+                by those entities to protect your personal information from
+                unauthorised access and use.
+              </Text>
+              <br />
+              <Text textStyle="body">
+                Please note that we are based in Australia and predominantly
+                process your information in Australia, which may not provide
+                equivalent levels of data protection in the jurisdiction you are
+                located in.
+              </Text>
             </ListItem>
             <ListItem>
               <Text textStyle="heading.h2">
                 Transferring your information overseas
+              </Text>
+              <br />
+              <Text textStyle="body">
+                It is our aim to provide you with access to premium Services. To
+                do this, we use the information you provide to us for market
+                research so we can better understand your needs. We may use your
+                personal information to conduct direct marketing, such as
+                telemarketing and advertising via email, SMS or post, in order
+                tell you about our products and services.
+              </Text>
+              <br />
+              <Text textStyle="body">
+                Your personal information may be passed to other entities
+                (including third party entities) for promotional purposes,
+                including direct marketing. Those entities may use the personal
+                information we provide to them to market to you, including by
+                sending you information or contacting you (by telephone, post or
+                electronically) about our Services. These entities may be
+                associated with us, or be agents, contractors or allied
+                organisations. The type of personal information we use or
+                disclose will usually be limited to your name and contact
+                details. Importantly, we will not use any sensitive personal
+                information for marketing purposes without your express consent.
+              </Text>
+              <br />
+              <Text textStyle="body">
+                You may choose to opt out of our marketing activities at any
+                time by advising us via the unsubscribe function or other
+                contact information provided in any marketing you receive.
+                Alternatively, you may advise us by contacting us directly.
+              </Text>
+              <br />
+              <Text textStyle="body">
+                Please note that it may take up to 60 days to fully implement
+                your request not to receive marketing or promotional materials.
+                We apologise if you receive any materials during this period.
+              </Text>
+              <br />
+              <Text textStyle="body">
+                We will not undertake any marketing activities which would
+                amount to a breach of any legislation including the Do Not Call
+                Register Act 2006 (Cth) and the Spam Act 2003.
               </Text>
             </ListItem>
             <ListItem>
