a2f8f7396921b8b1bf9ce9038595942b0f2557b9
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-10-13 13:37:35 +0800

    adding caching cdn images to serviceworker

--- hearth-web/vite.config.ts
+++ hearth-web/vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const getCache = ({ name, pattern }: any) => ({
+  urlPattern: pattern,
+  handler: "CacheFirst" as const,
+  options: {
+    cacheName: name,
+    expiration: {
+      maxEntries: 500,
+      maxAgeSeconds: 60 * 60 * 24 * 365 * 2, // 2 years
+    },
+    cacheableResponse: {
+      statuses: [200],
+    },
+  },
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,7 +24,13 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}", ""],
+        runtimeCaching: [
+          getCache({
+            pattern: /^https:\/\/res.cloudinary.com/,
+            name: "hearth-images",
+          }),
+        ],
       },
     }),
   ],
