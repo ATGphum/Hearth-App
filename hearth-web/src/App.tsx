c9f49340c1b1a527fa3ef854e9bbc8fbb072f8b3
Jeremy Vuong <jeremyvuong.dshs@gmail.com>
2023-10-16 16:18:59 +0800

    refactored app

--- hearth-web/src/App.tsx
+++ hearth-web/src/App.tsx
@@ -1,58 +1,62 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import "./App.css";
 import Tree from "./components/Tree";
-import viteEnv from "./config/vite-env";
-import { useEffect } from "react";
 import { initialiseAnalytics } from "./core/analytics";
-import { IsStandalone, getInstallableStatus } from "./core/helpers";
 import LoadingPage from "./pages/LoadingPage";
 
-const DesktopPage = lazy(() => import("./pages/DesktopPage"));
-const InstallationPage = lazy(() => import("./pages/InstallationMobilePage"));
-const WrongBrowserInstructionPage = lazy(
-  () => import("./pages/WrongBrowserInstructionPage")
-);
+// const DesktopPage = lazy(() => import("./pages/DesktopPage"));
+// const InstallationPage = lazy(() => import("./pages/InstallationMobilePage"));
+// const WrongBrowserInstructionPage = lazy(
+//   () => import("./pages/WrongBrowserInstructionPage")
+// );
 const AppContextProviders = lazy(() => import("./context/AppContextProviders"));
 
 function App() {
-  const installable = getInstallableStatus();
-  const isStandalone = IsStandalone();
+  // const installable = getInstallableStatus();
+  // const isStandalone = IsStandalone();
 
   useEffect(() => {
     initialiseAnalytics();
   }, []);
-  if (installable === "installable" || viteEnv.environment === "development") {
-    if (isStandalone || viteEnv.environment === "development") {
-      return (
-        <Suspense fallback={<LoadingPage />}>
-          <AppContextProviders>
-            <Tree />
-          </AppContextProviders>
-        </Suspense>
-      );
-    } else {
-      return (
-        <Suspense fallback={<LoadingPage />}>
-          <InstallationPage />
-        </Suspense>
-      );
-    }
-  }
-  if (installable === "non-installable") {
-    return (
-      <Suspense fallback={<LoadingPage />}>
-        <AppContextProviders>
-          <WrongBrowserInstructionPage />
-        </AppContextProviders>
-      </Suspense>
-    );
-  }
-
   return (
     <Suspense fallback={<LoadingPage />}>
-      <DesktopPage />
+      <AppContextProviders>
+        <Tree />
+      </AppContextProviders>
     </Suspense>
   );
+  // if (installable === "installable" || viteEnv.environment === "development") {
+  //   if (isStandalone || viteEnv.environment === "development") {
+  //     return (
+  //       <Suspense fallback={<LoadingPage />}>
+  //         <AppContextProviders>
+  //           <Tree />
+  //         </AppContextProviders>
+  //       </Suspense>
+  //     );
+  //   } else {
+  //     return (
+  //       <Suspense fallback={<LoadingPage />}>
+  //         <InstallationPage />
+  //       </Suspense>
+  //     );
+  //   }
+  // }
+  // if (installable === "non-installable") {
+  //   return (
+  //     <Suspense fallback={<LoadingPage />}>
+  //       <AppContextProviders>
+  //         <WrongBrowserInstructionPage />
+  //       </AppContextProviders>
+  //     </Suspense>
+  //   );
+  // }
+
+  // return (
+  //   <Suspense fallback={<LoadingPage />}>
+  //     <DesktopPage />
+  //   </Suspense>
+  // );
 }
 
 export default App;
